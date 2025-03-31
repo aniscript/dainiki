e9bbc6cd51e1e22f3e1cf622272de07b7dfdff6b
aneeslml <aneeslml@gmail.com>
2025-03-31 12:53:15 +0100

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 const Header = () => {
   return (
     <div className="absolute top-0 left-0">
-      <div className="flex justify-between items-stretch w-screen py-4 px-10">
-        <div>left</div>
-        <div>right</div>
+      <div className="flex justify-between items-center w-screen py-4 px-10">
+        <div className="text-indigo-900 text-3xl font-extrabold">Dainiki</div>
+        <div className="h-8 w-8 bg-amber-200 rounded-full flex items-center justify-center shadow-sm">
+          <span className="text-indigo-900 font-semibold text-sm">AL</span>
+        </div>
       </div>
     </div>
   );
