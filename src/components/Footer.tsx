46f94e349b29bcfd8c8723c5bfc5f14e06bc0393
aneeslml <aneeslml@gmail.com>
2025-03-31 13:06:13 +0100

    add icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,17 @@
+import { ListTodo, Music } from "lucide-react";
 import Quotes from "./Quotes";
 
 const Footer = () => {
   return (
     <div className="absolute bottom-0 left-0">
-      <div className="flex justify-between items-stretch w-screen py-4 px-6">
+      <div className="flex justify-between items-center w-screen py-4 px-6">
         <div>
           <Quotes />
         </div>
-        <div>right</div>
+        <section className="flex gap-2">
+          <ListTodo />
+          <Music />
+        </section>
       </div>
     </div>
   );
