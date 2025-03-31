7399861c80bee1dc398cc23b3c065b32a03d96cb
aneeslml <aneeslml@gmail.com>
2025-03-31 12:56:13 +0100

    display quotes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import Quotes from "./Quotes";
+
 const Footer = () => {
   return (
     <div className="absolute bottom-0 left-0">
-      <div className="flex justify-between items-stretch w-screen py-4 px-10">
-        <div>left</div>
+      <div className="flex justify-between items-stretch w-screen py-4 px-6">
+        <div>
+          <Quotes />
+        </div>
         <div>right</div>
       </div>
     </div>
---
+++ src/components/Quotes.tsx
@@ -0,0 +1,9 @@
+const Quotes = () => {
+  return (
+    <p className="text-indigo-600 italic">
+      "The old ways won't open new doors"
+    </p>
+  );
+};
+
+export default Quotes;
